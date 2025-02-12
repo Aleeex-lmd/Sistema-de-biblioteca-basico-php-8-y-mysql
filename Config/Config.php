8a678e4ded2732b3c07428651137b801a0ff1e33
Alex <sanchezgomez158@gmail.com>
2025-02-12 09:49:09 +0100

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -3,6 +3,6 @@ define('base_url',getenv('BASE_URL'));
 define('host',getenv('DB_HOST'));
 define('user',getenv('DB_USER'));
 define('pass',getenv('DB_PASS'));
-define('db',getenv('DB'));
+define('db',getenv('DB_NAME'));
 const charset = "charset=utf8";
 ?>
